fb1f33a46c70327fcc835fbb4f1e9616a2881b21
nickperson <ridwanhanif168@gmail.com>
2026-04-16 01:15:30 +0700

    change client side server

--- index.js
+++ index.js
@@ -89,7 +89,11 @@ const client = new Client({
             '--no-sandbox',
             '--disable-setuid-sandbox',
             '--disable-dev-shm-usage',
-            '--single-process'
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process', // Sangat penting untuk server RAM kecil
+            '--disable-gpu'
         ],
         // Jika jalan di Railway, biarkan undefined agar Puppeteer mencari Chromium bawaannya otomatis.
         // Jika di lokal, tetap gunakan Brave.
